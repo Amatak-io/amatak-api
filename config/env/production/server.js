711694a7c99b53d6a9d32ac92dc6bf82a32d081b
ronyman.com <ronyman.com@outlook.com>
2023-11-24 18:57:11 +1100

    update host

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,8 +1,14 @@
-
 module.exports = ({ env }) => ({
-    proxy: true,
-    url: env('APP_URL'), // Sets the public URL of the application.
-    app: { 
-      keys: env.array('APP_KEYS')
+  proxy: true,
+  host: "0.0.0.0",
+  port: process.env.PORT,
+  url: env('https://amatak-api.pages.dev'),
+  app: { 
+    keys: env.array('APP_KEYS')
+  },
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET'),
     },
-});
+  },
+})
